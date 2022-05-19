a9f023fb348a422a1e47351e26a6fd19663432ab
Badal Singh <99181462+cloud-badal@users.noreply.github.com>
2022-05-19 17:37:59 +0530

    Delete predict.py

--- services/fastapi-inference/predict.py
+++
@@ -1,7 +0,0 @@
-import logging
-logger = logging.getLogger(__name__)
-
-
-def predict(param1: float, param2: float):
-    logger.info(f"Predict function called with {param1}, {param2}")
-    return "I won't tell you."
