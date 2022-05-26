6a5321209660129f2950774e80ad52753fe8ef8a
Badal Singh <99181462+cloud-badal@users.noreply.github.com>
2022-05-26 21:57:03 +0530

    Update main.py

--- services/streamlit-inference/main.py
+++ services/streamlit-inference/main.py
@@ -1,4 +1,33 @@
-import streamlit as st
+from inspect import signature, getmembers, isfunction
+import gradio as gr
+try:
+    import ${module_name} as functions
+except ImportError as error:
+    print("Failed to import function ${module_name}: " + error.message)
+    raise error
 
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+
+def get_input(param_name, annotation):
+    if annotation is None:
+        return gr.Textbox(label=param_name)
+    elif annotation in [int, float]:
+        return gr.Number(label=param_name)
+    elif annotation == str:
+        return gr.Textbox(label=param_name)
+    else:
+        raise RuntimeError(f"Unsupported type {annotation}.")
+
+
+with gr.Blocks() as demo:
+    with gr.Tabs():
+        for name, func in getmembers(functions, predicate=isfunction):
+            with gr.TabItem(name):
+                sig = signature(func)
+                inputs = []
+                for param_name, value in sig.parameters.items():
+                    text_input = get_input(param_name, value.annotation)
+                    print(text_input)
+                    inputs.append(text_input)
+                button = gr.Button("Submit")
+                output = gr.Textbox(label="Output", interactive=False)
+                button.click(func, inputs=inputs, outputs=[output])
